4bac57d62a8e1a180be00f31b3557286d1c71696
leehyowon <leehyowon@selim.kr>
2023-08-17 17:54:37 +0900

    2023 08 17 3강 jsx 문법

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+let a = 10;
+const b = 20;
+
 function App() {
-  return <div>안녕2</div>;
+  let c;
+  let d = undefined;
+  console.log(1, c);
+
+  const mystyle = {
+    color: 'red',
+  };
+
+  return (
+    <div>
+      <div style={mystyle}>안녕 {a === 10 ? '10입니다' : '10이 아닙니다'}</div>
+      <h1 className="box-style">해딩태그 {b === 20 && '20입니다'}</h1>
+      <hr />
+    </div>
+  );
 }
 
 export default App;
