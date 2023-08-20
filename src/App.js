b87632293cfb28e79709720db9e3e9fc0238b7c6
hyowon <gydnjs2007@naver.com>
2023-08-20 16:05:08 +0900

    2023 08 20 4강 불변함수

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-let a = 10;
-const b = 20;
-
 function App() {
-  let c;
-  let d = undefined;
-  console.log(1, c);
-
-  const mystyle = {
-    color: 'red',
-  };
+  let list = [1, 2, 3]; // 이 값에 서버에서 restcontroller로 받아온 데이터를 저장하면 화면에 데이터를 뿌릴수 있을것
+  //그러니 이 list 값만 관리 하면되겠지
 
   return (
     <div>
-      <div style={mystyle}>안녕 {a === 10 ? '10입니다' : '10이 아닙니다'}</div>
-      <h1 className="box-style">해딩태그 {b === 20 && '20입니다'}</h1>
-      <hr />
+      <div>
+        {list.map((n) => {
+          return <h1>{n}</h1>;
+        })}
+      </div>
+      <div>
+        {list.map((n) => {
+          return n;
+        })}
+      </div>
     </div>
   );
 }
