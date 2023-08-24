709e81697ab0f506e00f4fc82346b205c1250e19
leehyowon <leehyowon@selim.kr>
2023-08-24 17:41:32 +0900

    minor commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 import { useState } from 'react';
-import Sub from './Sub';
+import './App.css';
 
 function App() {
-  //let number = 1;
-  const [number, setNumber] = useState(1);
+  console.log('App실행됨');
 
-  const add = () => {
-    setNumber(number + 1);
-    console.log('add', number);
+  let sample = [
+    { id: 1, name: 'kim1' },
+    { id: 2, name: 'kim2' },
+  ];
+
+  const [users, setUsers] = useState(sample);
+
+  const download = () => {
+    setUsers(sample); // 이렇게 일땐 users와 sample은 똑같은 데이터이지만 서로 주소가 다름
+    //그래서 다른 데이터로 인식해 재랜더링 됨
+    //만약 최초 데이터와 값, 주소값이 서로 같다면 재랜더링 안됨
+
+    //setUsers([...sample]); // 이렇게 하면 주소값이 다르니 재랜더링 될 것
   };
 
   return (
     <div>
-      <div>
-        <h1>숫자 : {number}</h1>
-        <button onClick={add}>더하기</button>
-        <Sub />
-      </div>
+      <button onClick={download}>download</button>
+      {users.map((user) => (
+        <h1>
+          {user.id}, {user.name}
+        </h1>
+      ))}
     </div>
   );
 }
