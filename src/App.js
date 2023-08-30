d243a03c320dede27660f9d41c0596d377897d99
hyowon <gydnjs2007@naver.com>
2023-08-30 18:40:28 +0900

    minor commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     setData(downloadData);
   };
 
-  //실행시점:
+  // useEffect 실행시점:
   // 1. 최초 렌더링 시
   // 2. state 변수 변경시 (useEffect의 dependency list를 지정하면
   //    해당 state값이 변할때만 useEffect 실행)
