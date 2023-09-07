55449ca83a7c23fa8574d57735cd5f4b21aaccd2
leehyowon <leehyowon@selim.kr>
2023-09-07 18:07:34 +0900

    minor commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { createRef, useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
+import { styled } from 'styled-components';
 
-const a = {
-  backgroundColor: 'red',
-};
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #bf4f74;
+`;
 
 function App() {
   return (
     <div>
-      <div style={a}>안녕</div>
-      <div className="box-style">hi</div>
+      <Title>안녕</Title>
     </div>
   );
 }
