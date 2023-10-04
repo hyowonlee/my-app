a91b5c30ec172905042a06b0ced8316a635a6fc2
leehyowon <leehyowon@selim.kr>
2023-10-04 17:45:44 +0900

    2023 10 04 13강 styled-component 스타일 상속

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,12 +5,17 @@ const StyledDeleteButton = styled.button`
   color: ${(props) => (props.user.username === 'ssar' ? 'blue' : 'red')};
 `;
 
+const StyledAddButton = styled(StyledDeleteButton)`
+  background-color: green;
+`;
+
 const Home = (props) => {
   //구조분할 할당 (이런식으로 대입해주면 props 안에있는 key 값과 비교해서 같은 변수에 데이터가 자동으로 값이 들어간다)
   const { boards, setBoards, number, user, setNumber, increaseNumber } = props;
 
   return (
     <div>
+      <StyledAddButton user={user}>더하기</StyledAddButton>
       <h1>홈페이지 입니다. 번호 {number}</h1>
       <button
         onClick={() => {
