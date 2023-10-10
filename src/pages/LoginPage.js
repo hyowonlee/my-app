f570c75377f106c9ac074faae47ad52c2c1f2b55
leehyowon <hyowon.lee@selim.kr>
2023-10-10 18:06:05 +0900

    minor commit

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Login from '../components/login/Login';
 import Footer from '../components/Footer';
-import { useMatch } from 'react-router';
+import { useMatch, useNavigate, useNavigation } from 'react-router-dom';
 
 //components 폴더 안의 컴포넌트들을 합쳐서 로그인 페이지를 만드는 컴포넌트를 생성
 //메인 페이지에선 이 컴포넌트만 작성해주면 안에 있는 3개 컴포넌트를 한번에 생성
@@ -12,7 +12,14 @@ const LoginPage = () => {
   console.log(match);
   console.log(match.pathname);
   console.log(match.params.id);
-  return <Login></Login>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <button onClick={() => navigate(-1)}> 뒤로가기 </button>
+      <Login />
+    </div>
+  );
 };
 
 export default LoginPage;
